1a417365af06074a58ca4ec095ab85d326136e3e
Udit Shukla <75034362+Udit-Shukla@users.noreply.github.com>
2024-06-15 01:28:26 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "rexcodes",
-    project: "javascript-react"
-  })],
+  plugins: [react(), ],
 
   build: {
     sourcemap: true
